2c1360ad418b8965d6a8cd8d79c3a62dbecc3899
metleeha <nodongth@gmail.com>
2020-07-25 12:00:29 +0900

    Add: auth reducer

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,20 +1,50 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+// actions 
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+    CHANGE_FIELD,
+    ({form, key, value}) => ({
+        form, // register, login 
+        key, //username, password, passwordConfirm
+        value, // 실제 바꾸려는 값
+    }),
+);
+
+export const initializeForm = createAction(INITIALIZE_FORM, form => form); //register / login
+
+const initialState = {
+    register: {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+    },
+    login: {
+        username: '',
+        password: '',
+    }
+}
+
+const auth = handleActions(
+    {
+        [CHANGE_FIELD]: (state, { payload: { form, key, value } }) => 
+          produce(state, draft => {
+              draft[form][key] = value;
+          }),
+        [INITIALIZE_FORM]: (state, { payload: form }) => ({
+            ...state,
+            [form]: initialState[form]
+        })
+    },
+    initialState,
+);
+
 
 export function* authSaga() {
     // pass    
 }
-
-const initialState = {};
-
-const auth = handleActions(
-    {
-        [SAMPLE_ACTION]: (state, action) => state,
-    },
-    initialState,
-);
 
 export default auth;
