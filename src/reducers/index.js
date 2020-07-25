44c063ca1911bf9e24234ad50399faeab737e2c3
metleeha <nodongth@gmail.com>
2020-07-25 12:17:49 +0900

    Add: config axios & loading reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import { all } from 'redux-saga/effects';
 import auth, { authSaga } from './auth';
+import loading from './loading';
 
 const rootReducer = combineReducers({
     auth,
+    loading
 });
 
 export function* rootSaga() {
